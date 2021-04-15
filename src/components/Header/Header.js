1fe70ea4ad605597c2772f2e7039a3e7c8175715
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-15 08:57:38 +0530

    heder

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,26 @@
-import React from 'react'
-import './Header.css';
+import { IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import { IconButton} from '@material-ui/core';
+import React from 'react';
+import './Header.css';
 
 function Header() {
-    return (
-        <div className='header' >
-             <h1>Hello React Redux , im back!!!</h1>
-             <div className="header__left">
-                 <IconButton >
-                 <MenuIcon />
-                 </IconButton>
+	return (
+		<div className='header'>
+			<div className='header__left'>
+				<IconButton>
+					<MenuIcon />
+				</IconButton>
 
-                 <img src="https://logodownload.org/wp-content/uploads/2018/03/gmail-logo-0.png" alt=""/>
-
-             </div>
-             <div className="header__middle">
-
-             </div>
-             <div className="header__right">
-
-             </div>
-        </div>
-    )
+				<img
+                    className='gmail__logo'
+					src='https://logodownload.org/wp-content/uploads/2018/03/gmail-logo-0.png'
+					alt=''
+				/>
+			</div>
+			<div className='header__middle'></div>
+			<div className='header__right'></div>
+		</div>
+	);
 }
 
-export default Header
+export default Header;
