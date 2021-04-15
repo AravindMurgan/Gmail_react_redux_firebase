7b3bdc658ab7544792720b2a189e1665ff7b5999
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-15 08:00:52 +0530

    onto-header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React Redux , im back!!!</h1>
+     
       <Header />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
     return (
         <div>
-            
+             <h1>Hello React Redux , im back!!!</h1>
         </div>
     )
 }
