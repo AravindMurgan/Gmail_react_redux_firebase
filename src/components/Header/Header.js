58177ef588e307563a1fbda3d426f9f3aa684abd
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-15 11:48:17 +0530

    header-done --onto sidebar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
-import { IconButton } from '@material-ui/core';
+import { IconButton ,Avatar} from '@material-ui/core';
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 import React from 'react';
 import './Header.css';
-import SearchIcon from '@material-ui/icons/Search';
-import ArrowDropDown from '@material-ui/icons/ArrowDropDown';
-
 
 function Header() {
 	return (
@@ -15,17 +16,28 @@ function Header() {
 				</IconButton>
 
 				<img
-                    className='gmail__logo'
+					className='gmail__logo'
 					src='https://cdn.freelogovectors.net/wp-content/uploads/2018/04/gmail-logo.png'
 					alt=''
 				/>
 			</div>
 			<div className='header__middle'>
-                <SearchIcon />
-                <input type="text"/>
-                <ArrowDropDown />
-            </div>
-			<div className='header__right'></div>
+				<SearchIcon />
+				<input type='text' placeholder='Search mail' />
+				<ArrowDropDown className='header__inputCaret' />
+			</div>
+			<div className='header__right'>
+				<IconButton>
+					<AppsIcon />
+				</IconButton>
+
+                <IconButton>
+					<NotificationsIcon />
+				</IconButton>
+
+                <Avatar />
+
+			</div>
 		</div>
 	);
 }
