280488fe38ea6fef75a521378c28975db53492f5
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-15 08:28:37 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
     return (
         <div>
              <h1>Hello React Redux , im back!!!</h1>
+             <div className="header__left">
+
+             </div>
+             <div className="header__middle">
+
+             </div>
+             <div className="header__right">
+
+             </div>
         </div>
     )
 }
