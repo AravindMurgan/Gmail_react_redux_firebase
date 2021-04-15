60c02e6f4144b6720eaa6eff80386543d01f39db
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-15 08:41:57 +0530

    header__left

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
+import { IconButton} from '@material-ui/core';
 
 function Header() {
     return (
-        <div>
+        <div className='header' >
              <h1>Hello React Redux , im back!!!</h1>
              <div className="header__left">
+                 <IconButton >
+                 <MenuIcon />
+                 </IconButton>
+
+                 <img src="https://logodownload.org/wp-content/uploads/2018/03/gmail-logo-0.png" alt=""/>
 
              </div>
              <div className="header__middle">
