de53804f5f3846d4a5b3af852a13dbd925e52312
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-15 12:33:02 +0530

    compose-btn

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,13 @@
+import { Button } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import React from 'react';
-import  './Sidebar.css';
-
+import './Sidebar.css';
 function Sidebar() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<div className='sidebar'>
+			<Button startIcon={<AddIcon />}>Compose</Button>
+		</div>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
