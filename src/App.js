771f045abb187f8fddb6372db3d5a6fc2e4d25ee
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-24 12:15:44 +0530

    router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
-  return (
-    <div className="App">
-     
-      <Header />
-
+	return (
+		<div className='App'>
+			<Header />
       <Sidebar />
-    </div>
-  );
+      
+		</div>
+	);
 }
 
 export default App;
