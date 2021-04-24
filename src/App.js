974d6cd7e731b1515263ab8416757dbff7bbb6e4
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-04-24 13:39:24 +0530

    emailist component--setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import EmailList from './components/AppBody/EmailList';
+import Mail from './components/AppBody/Mail';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
-
 function App() {
 	return (
-		<div className='App'>
-			<Header />
-      <Sidebar />
-      
-		</div>
+		<Router>
+			<div className='App'>
+				<Header />
+
+				<div className='app__body'>
+					<Sidebar />
+
+					<Switch>
+						<Route path='/mail'>
+							<Mail />
+						</Route>
+						<Route path='/'>
+							<EmailList />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
 	);
 }
 
---
+++ src/components/AppBody/EmailList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './EmailList.css';
+
+function EmailList() {
+    return (
+        <div className='emaillist'>
+            <div className="emailList__settings">
+            <div className="emailList__settingsLeft">
+            
+            </div>
+
+            <div className="emailList__settingRight">
+            
+            </div>    
+            </div>    
+        </div>
+    )
+}
+
+export default EmailList
